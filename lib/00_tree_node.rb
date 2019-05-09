0a050c710c209c79d33f4ceb05003e7afe2237bb
iampeternguyen <ptnguyen1987@gmail.com>
2019-05-09 21:35:20 +0700

    add parent= method

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,22 @@
 class PolyTreeNode
+  attr_reader :parent, :children, :value
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
 
+  def parent=(parent)
+    if parent.nil?
+      return @parent = nil
+    elsif @parent && @parent != parent
+      @parent.children.slice!(@parent.children.index(self))
+    end
+    @parent = parent
+    @parent.children.push(self) unless @parent.children.include?(self)
+  end
+
+  def add_child
+
+  end
 end
